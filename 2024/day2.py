8a454912b9e69f0366eb776414018d2c807818f7
Basile Rommes <basilerommes@hotmail.com>
2024-12-05 00:29:54 +0100

    day2 part 1 solved; reformated indentation of all .py files

--- 2024/day2.py
+++ 2024/day2.py
@@ -3,15 +3,19 @@ import sys
 
 import argparse
 
+
 def main():
     # Initialize parser
-    parser = argparse.ArgumentParser(description='Process input file type and debug flag')
-    
+    parser = argparse.ArgumentParser(
+        description="Process input file type and debug flag"
+    )
+
     # Add arguments
-    parser.add_argument('input_suffix', #choices=['in', 'example'],
-                      help='Suffix of input file to process')
-    parser.add_argument('--debug', action='store_true',
-                      help='Enable debug mode')
+    parser.add_argument(
+        "input_suffix",  # choices=['in', 'example'],
+        help="Suffix of input file to process",
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
 
     # Parse arguments
     try:
@@ -20,77 +24,83 @@ def main():
         # Handle the case where argparse displays help or errors
         sys.exit(1)
 
-    with open(f'inputs/02.{args.input_suffix}', 'r') as file:
-      input = file.read()
+    with open(f"inputs/02.{args.input_suffix}", "r") as file:
+        input = file.read()
 
     if args.debug:
-      print(input)
+        print(input)
 
     safe_lines = 0
     unsafe_found = False
-    for line in input.split('\n'):
-      if not line:  # or if line == ''
-        continue
-      if args.debug:
-        print(line)
-      # check if decresing or increasing
-      level_list = [int(level) for level in line.split(' ')]
-      level_prev = level_list[0]
-      level = level_list[1]
+    for line in input.split("\n"):
+        if not line:  # or if line == ''
+            continue
+        if args.debug:
+            print(line)
+        # check if decresing or increasing
+        level_list = [int(level) for level in line.split(" ")]
+        level_prev = level_list[0]
+        level = level_list[1]
 
-      unsafe = False
+        unsafe = False
 
-      decreasing = False
-      increasing = False
+        decreasing = False
+        increasing = False
 
-      # def iter
+        # def iter
 
-      # note: range is right exclusive!
-      if level in range(level_prev-3-1, level_prev): # e.g. "7,6" then 6 is in range(7-4, 7) -> yes so decreasing
-        decreasing = True
-      elif level in range(level_prev+1, level_prev+3+1):  # e.g. "6,7" then 7 is in range(6+1, 6+4) -> yes so increasing
-        increasing = True
-      else:
-         continue
-
-      if decreasing:
-        for i in range(2, len(level_list)):
-          level = level_list[i]
-          level_prev = level_list[i-1]
-
-          if level_list[i] not in range(level_prev-3-1, level_prev):
+        # note: range is right exclusive!
+        if level in range(
+            level_prev - 3, level_prev
+        ):  # e.g. "7,6" then 6 is in range(7-3, 7) -> yes so decreasing
+            decreasing = True
+        elif level in range(
+            level_prev + 1, level_prev + 3 + 1
+        ):  # e.g. "6,7" then 7 is in range(6+1, 6+4) -> yes so increasing
+            increasing = True
+        else:
             if args.debug:
-             print("unsafe")
-            unsafe = True
-            break
-        # if we go through the loop whitout breaking, it means it was all decreasing in a safe amount
-        if not unsafe:
-          if args.debug:
-            print("safe")
-          safe_lines += 1
+                print("unsafe")
+            continue
 
-      if increasing:
-        for i in range(2, len(level_list)):
-          level = level_list[i]
-          level_prev = level_list[i-1]
+        if decreasing:
+            for i in range(2, len(level_list)):
+                level = level_list[i]
+                level_prev = level_list[i - 1]
 
-          if level_list[i] not in range(level_prev+1, level_prev+3+1):
-            if args.debug:
-               print("unsafe")
-            unsafe = True
-            break
+                if level_list[i] not in range(level_prev - 3, level_prev):
+                    if args.debug:
+                        print("unsafe")
+                    unsafe = True
+                    break
+            # if we go through the loop whitout breaking, it means it was all decreasing in a safe amount
+            if not unsafe:
+                if args.debug:
+                    print("safe")
+                safe_lines += 1
 
-        # if unsafe_found: # guarantees a double break, so we go to 
-        #     break
+        if increasing:
+            for i in range(2, len(level_list)):
+                level = level_list[i]
+                level_prev = level_list[i - 1]
 
-        # if we go through the loop whitout breaking, it means it was all decreasing in a safe amount
-        if not unsafe:
-          if args.debug:
-            print("safe")
-          safe_lines += 1
-    
+                if level_list[i] not in range(level_prev + 1, level_prev + 3 + 1):
+                    if args.debug:
+                        print("unsafe")
+                    unsafe = True
+                    break
+
+            # if unsafe_found: # guarantees a double break, so we go to
+            #     break
+
+            # if we go through the loop whitout breaking, it means it was all decreasing in a safe amount
+            if not unsafe:
+                if args.debug:
+                    print("safe")
+                safe_lines += 1
+
     print(safe_lines)
-      
+
+
 if __name__ == "__main__":
     main()
-
--- 2024/day3.py
+++ 2024/day3.py
@@ -3,18 +3,23 @@ import sys
 
 import argparse
 
-def mul(X,Y):
-  return X*Y
+
+def mul(X, Y):
+    return X * Y
+
 
 def main():
     # Initialize parser
-    parser = argparse.ArgumentParser(description='Process input file type and debug flag')
-    
+    parser = argparse.ArgumentParser(
+        description="Process input file type and debug flag"
+    )
+
     # Add arguments
-    parser.add_argument('input_suffix', #choices=['in', 'example'],
-                      help='Suffix of input file to process')
-    parser.add_argument('--debug', action='store_true',
-                      help='Enable debug mode')
+    parser.add_argument(
+        "input_suffix",  # choices=['in', 'example'],
+        help="Suffix of input file to process",
+    )
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
 
     # Parse arguments
     try:
@@ -25,54 +30,58 @@ def main():
 
     # with open('inputs/03.in', 'r') as file:
     # with open('inputs/03.example', 'r') as file:
-    with open(f'inputs/03.{args.input_suffix}', 'r') as file:
-      input = file.read()
+    with open(f"inputs/03.{args.input_suffix}", "r") as file:
+        input = file.read()
 
     if args.debug:
-      print(input)
+        print(input)
 
-    match_list = re.findall(pattern=r"mul\([0-9]?[0-9]?[0-9]\,[0-9]?[0-9]?[0-9]\)", string=input)
+    match_list = re.findall(
+        pattern=r"mul\([0-9]?[0-9]?[0-9]\,[0-9]?[0-9]?[0-9]\)", string=input
+    )
 
     if args.debug:
-      print(match_list)
+        print(match_list)
 
     result = 0
     for item in match_list:
-      result += eval(item)
-      if args.debug:
-        print(item)
-        print(eval(item))
+        result += eval(item)
+        if args.debug:
+            print(item)
+            print(eval(item))
 
     print("Part 1")
     print(result)
 
-    # ?: at the beginning of paranthesis is defining a non-capturing group. This means that you have a group that needs to be matched as a whole and can be modified (by '?' at the end) but it doesn't need to 
-    match_list = re.findall(pattern=r"mul\([0-9]?[0-9]?[0-9]\,[0-9]?[0-9]?[0-9]\)|do(?:n't)?\(\)", string=input)
+    # ?: at the beginning of paranthesis is defining a non-capturing group. This means that you have a group that needs to be matched as a whole and can be modified (by '?' at the end) but it doesn't need to
+    match_list = re.findall(
+        pattern=r"mul\([0-9]?[0-9]?[0-9]\,[0-9]?[0-9]?[0-9]\)|do(?:n't)?\(\)",
+        string=input,
+    )
     # match_list = re.findall(pattern=r"mul\([0-9]?[0-9]?[0-9]\,[0-9]?[0-9]?[0-9]\)", string=input)
 
     if args.debug:
-      print(match_list)
+        print(match_list)
 
     result = 0
     do = True
     for item in match_list:
-      if item == "do()":
-        do = True
-        if args.debug:
-          print(item)  
-      elif item == "don't()":
-        do = False
-        if args.debug:
-          print(item)
-      elif do:
-        result += eval(item)
-        if args.debug:
-          print(item)
-          print(eval(item))
+        if item == "do()":
+            do = True
+            if args.debug:
+                print(item)
+        elif item == "don't()":
+            do = False
+            if args.debug:
+                print(item)
+        elif do:
+            result += eval(item)
+            if args.debug:
+                print(item)
+                print(eval(item))
 
     print("Part 2")
     print(result)
-
 
 
 if __name__ == "__main__":
